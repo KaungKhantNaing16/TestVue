0eed3ad8911a3e9ca235241f0ee8cf7cc1e40b0a
KaungKhantNaing16 <scm.kaungkhantnaing@gmail.com>
2022-05-25 14:35:32 +0630

    [laravel-vue]Add company CRUD design and functions

--- app/Http/Requests/CompanyRequest.php
+++ app/Http/Requests/CompanyRequest.php
@@ -24,7 +24,7 @@ class CompanyRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => ['required'],
+            'name' => ['required', 'alpha'],
             'email' => ['required', 'email'],
             'address' => ['string'],
             'website' => ['url']
--- resources/js/components/companies/CompaniesIndex.vue
+++ resources/js/components/companies/CompaniesIndex.vue
@@ -1,4 +1,9 @@
 <template>
+    <div class="flex place-content-end mb-4">
+        <div class="px-4 py-2 text-white bg-indigo-600 hover:bg-indigo-700 cursor-pointer">
+            <router-link :to="{ name: 'companies.create' }" class="text-sm font-bold">Create company</router-link>
+        </div>
+    </div>
     <div class="overflow-hidden overflow-x-auto min-w-full align-middle sm:rounded-md">
         <table class="min-w-full border divide-y divide-gray-200">
             <thead>
@@ -36,6 +41,10 @@
                         {{ item.website }}
                     </td>
                     <td class="px-6 py-4 text-sm leading-5 text-gray-900 text-center whitespace-no-wrap">
+                        <router-link :to="{ name: 'companies.edit', params: { id: item.id } }"
+                                     class="mr-2 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
+                            Edit
+                        </router-link>
                         <button @click="deleteCompany(item.id)"
                                 class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
                             Delete
